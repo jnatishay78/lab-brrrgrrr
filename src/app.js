22846cb9fbe43f0eb7b73642c1c533fcfb5e9095
jnatishay78 <atishayjain070401@gmail.com>
2022-05-18 02:13:14 +0530

    VITFED15

--- src/app.js
+++ src/app.js
@@ -7,63 +7,63 @@ var wholeWheatBun = 10;
 //Ingredients of the burger along with the price
 // Clue: the name is same as the textcontent of the button. Will be useful later on :)
 var ingredients = {
-  Patty: 80,
-  Cheese: 10,
-  Tomatoes: 20,
-  Onions: 20,
-  Lettuce: 20
+    Patty: 80,
+    Cheese: 10,
+    Tomatoes: 20,
+    Onions: 20,
+    Lettuce: 20
 };
 
 //Current state of the ingredients in the burger
 var state = {
-  Patty: true,
-  Cheese: true,
-  Tomatoes: true,
-  Onions: true,
-  Lettuce: true
+    Patty: true,
+    Cheese: true,
+    Tomatoes: true,
+    Onions: true,
+    Lettuce: true
 };
 
 // This function renders the entire screen everytime the state changes accordingly
 function renderAll() {
-  renderPatty();
-  renderCheese();
-  renderTomatoes();
-  renderOnions();
-  renderLettuce();
-  renderButtons();
-  renderIngredientsBoard();
-  renderPrice();
+    renderPatty();
+    renderCheese();
+    renderTomatoes();
+    renderOnions();
+    renderLettuce();
+    renderButtons();
+    renderIngredientsBoard();
+    renderPrice();
 }
 
 function renderPatty() {
-  let patty = document.querySelector("#patty");
-  //you can also use getElementById
-  if (state.Patty) {
-    patty.style.display = "inherit";
-  } else {
-    patty.style.display = "none";
-  }
+    let patty = document.querySelector("#patty");
+    //you can also use getElementById
+    if (state.Patty) {
+        patty.style.display = "inherit";
+    } else {
+        patty.style.display = "none";
+    }
 }
 
 function renderCheese() {
-  //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
+    //Trial 1 - Change the visibility of cheese based on state by manipulating the DOM
 }
 
 function renderTomatoes() {
-  //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
+    //Trial 1 - Change the visibility of Tomatoes based on state by manipulating the DOM
 }
 
 function renderOnions() {
-  //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
+    //Trial 1 - Change the visibility of Onions based on state by manipulating the DOM
 }
 
 function renderLettuce() {
-  //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
+    //Trial 1 - Change the visibility of Lettuce based on state by manipulating the DOM
 }
 
-document.querySelector(".btn-patty").onclick = function () {
-  state.Patty = !state.Patty;
-  renderAll();
+document.querySelector(".btn-patty").onclick = function() {
+    state.Patty = !state.Patty;
+    renderAll();
 };
 
 // Trial 2 - Setup event listener for the cheese button
